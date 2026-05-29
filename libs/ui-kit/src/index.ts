81ab159aa16995b092c0b3c7c1d7f260304c07c9
omaryoussef160 <omaryoussef150@gmail.com>
2026-05-30 02:58:41 +0300

    feat(ui-kit): add ModalComponent - TASK-M-03

--- libs/ui-kit/src/index.ts
+++ libs/ui-kit/src/index.ts
@@ -1,3 +1,3 @@
 export { ButtonComponent } from './button/button.component';
 export { InputComponent }  from './input/input.component';
- 
+export { ModalComponent }  from './modal/modal.component';
