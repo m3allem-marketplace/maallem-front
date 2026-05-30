c5c9331ed49bebfb7cad5bfaa2a86f132b687b32
omaryoussef160 <omaryoussef150@gmail.com>
2026-05-30 03:06:27 +0300

    feat(ui-kit): add SpinnerComponent - TASK-M-05

--- libs/ui-kit/src/index.ts
+++ libs/ui-kit/src/index.ts
@@ -1,4 +1,5 @@
-export { ButtonComponent }         from './button/button.component';
-export { InputComponent }          from './input/input.component';
-export { ModalComponent }          from './modal/modal.component';
+export { ButtonComponent }                       from './button/button.component';
+export { InputComponent }                        from './input/input.component';
+export { ModalComponent }                        from './modal/modal.component';
 export { ToastService, ToastContainerComponent } from './toast/toast.service';
+export { SpinnerComponent }                      from './spinner/spinner.component';
