95474e1c6db49f4feb519136458eaf71ec72e3b0
abdoali2000 <aabodawali@gmail.com>
2026-06-02 00:40:00 +0300

    feat(core): TASK-L-08 implement global loading interceptor and counter service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
+
 @NgModule({
     providers: [
         {
@@ -9,11 +11,16 @@ import { ErrorInterceptor } from './interceptors/error.interceptor';
             useClass: AuthInterceptor,
             multi: true
         },
-    {
-        provide: HTTP_INTERCEPTORS,
-        useClass: ErrorInterceptor,
-        multi: true
-    }
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorInterceptor,
+            multi: true
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: LoadingInterceptor,
+            multi: true
+        }
     ]
 })
 export class CoreModule { }
